5165bef9be1768f1aa85f539aa68f643900caf64
Plinio Altoe <plinio.altoe@edirectinsure.com>
2020-05-01 16:37:40 +0100

    Create routes to make tests pass on NodeJS challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,77 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo);
+
+  return response.json(repo);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex(item => item.id === id);
+
+  if (index < 0)
+    return response.status(400).json({ error: `Project not found!` });
+
+  const repo = {
+    ...repositories[index],
+    title,
+    url,
+    techs,
+  };
+
+  repositories[index] = {
+    ...repositories[index],
+    title,
+    url,
+    techs,
+  };
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const index = repositories.findIndex(item => item.id === id);
+
+  if (index < 0)
+    return response.status(400).json({ error: `Project not found!` });
+
+  repositories.splice(index, 1);
+
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const index = repositories.findIndex(item => item.id === id);
+
+  if (index < 0)
+    return response.status(400).json({ error: `Project not found!` });
+
+  repositories[index].likes += 1;
+
+  return response.json(repositories[index]);
 });
 
 module.exports = app;
